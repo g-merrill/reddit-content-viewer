a1ce8d361b718bee648bd92dc34c26f159b0f5ae
Greg Merrill <gmerrill03@gmail.com>
2019-07-22 10:51:17 -0700

    updated js, reorganized files

--- post-viewer/js/main.js
+++ post-viewer/js/main.js
@@ -1,95 +1,186 @@
+// creates random Reddit-style data
+// initialize data object structure
 const myRedditJson = {
   data: {
     children: [
     ]
   }
 };
-const jsonChildren = myRedditJson.data.children
-for (let j = 0; j < 25; j++) {
+// assign variable representing individual post
+const jsonChildren = myRedditJson.data.children;
+
+// fill each post object key with randomized data
+for (let i = 0; i < 25; i++) {
   jsonChildren.push({data:{}});
+  let postData = jsonChildren[i].data;
+  postData.score = Math.floor(Math.random() * 60000);
+  postData.post_hint = Math.floor(Math.random() * 10);
+  postData.title = Math.random().toString(36).substring(2, 9) + " " + Math.random().toString(36).substring(2, 8);
+  postData.url = Math.random().toString(36).substring(2, 13) + ".com";
+  postData.subreddit_name_prefixed = "r/" + Math.random().toString(36).substring(2, 10);
+  postData.author = "u/" + Math.random().toString(36).substring(2, 7);
+  postData.created_utc = Math.floor(Math.random() * 23 + 2);
+  postData.num_comments = Math.floor(Math.random() * 2000);
 }
+// print data to console
+console.log("DATA:", jsonChildren);
 
-for (let k = 0; k < jsonChildren.length; k++) {
-  jsonChildren[k].data.score = Math.floor(Math.random() * 60) + "." + Math.floor(Math.random() * 10) + "k";
-  jsonChildren[k].data.title = Math.random().toString(36).substring(2, 9) + " " + Math.random().toString(36).substring(2, 8);
-  jsonChildren[k].data.post_hint = Math.floor(Math.random() * 10);
-  jsonChildren[k].data.url = Math.random().toString(36).substring(2, 13) + ".com";
-  jsonChildren[k].data.subreddit_name_prefixed = "r/" + Math.random().toString(36).substring(2, 10);
-  jsonChildren[k].data.author = "Posted by u/" + Math.random().toString(36).substring(2, 7);
-  jsonChildren[k].data.created_utc = Math.floor(Math.random() * 24 + 1) + " hours ago";
-  jsonChildren[k].data.num_comments = Math.floor(Math.random() * 2000);
-}
-console.log(jsonChildren);
+// attempt to build page dynamically using js
+const buildStructure = () => {
+  for (let i = 1; i <= jsonChildren.length; i++) {
+    let postContainer = document.createElement('div');
+    postContainer.id = "postContainer_" + i;
+    document.body.appendChild(postContainer);
 
-const userAction3 = function() {
-  document.body.setAttribute("style", "font-family:sans-serif");
-  // replace i < 1 with: i < jsonChildren.length
-  for (let i = 0; i < jsonChildren.length; i++) {
-    let postContent = jsonChildren[i].data;
-    
-    let postWrapper = document.createElement('div');
-    postWrapper.style = "height: 90px; border: solid";
-    postWrapper.id = "postContainer" + i;
-    document.body.appendChild(postWrapper);
+    for (let j = 1; j <= 4; j++) {
+      let postSection = document.createElement('div');
+      postSection.id = "postSection_" + i + "_" + j;
+      document.getElementById("postContainer_" + i).appendChild(postSection);
 
-    let div0 = document.createElement('div');
-    div0.innerHTML = postContent.score;
-    div0.style = "display: inline-block; height: 100%; width: 35px; font-size: 12px; position: relative; top: -60px; padding: 0 10px 0; border: solid";
-    document.getElementById('postContainer' + i).appendChild(div0);
-    
-    let postType;
-    if (postContent.post_hint > 6) {
-      postType = "img";
-    } else if (postContent.post_hint > 3) {
-      postType = "gif";
-    } else {
-      postType = "txt";
+      if (j===3) {
+        for (let k = 1; k <= 2; k++) {
+          let subSection = document.createElement('div');
+          subSection.id = "subDiv_" + i + "_" + j + "_" + k;
+          document.getElementById("postSection_" + i + "_" + j).appendChild(subSection);
+        }
+      }
     }
-    let div1 = document.createElement('div');
-    div1.innerHTML = postType;
-    div1.style = "display: inline-block; height: 90px; width: 35px; font-size: 12px; position: relative; top: -60px; border: solid";
-    document.getElementById('postContainer' + i).appendChild(div1);
-    
-    let div2 = document.createElement('div');
-    div2.id = "div2_" + i
-    div2.style = "display: inline-block; height: 90px; width: 350px; border: solid";
-    document.getElementById('postContainer' + i).appendChild(div2);
-    
-    let div2a = document.createElement('div');
-    div2a.id = "div2a_" + i;
-    div2a.style = "padding: 10px; border: solid";
-    document.getElementById("div2_" + i).appendChild(div2a);
-
-    
-    let div2a1 = document.createElement('div');
-    div2a1.innerHTML = postContent.title;
-    div2a1.style = "display: inline-block;";
-    document.getElementById('div2a_' + i).appendChild(div2a1);
-    
-    let div2a2 = document.createElement('div');
-    div2a2.innerHTML = postContent.url;
-    div2a2.style = "display: inline-block; padding: color: blue; font-size: 12px";
-    document.getElementById('div2a_' + i).appendChild(div2a2);
-    
-    let div2b = document.createElement('div');
-    div2b.id = "div2b_" + i;
-    div2b.style = "padding: 10px; border: solid";
-    document.getElementById("div2_" + i).appendChild(div2b);
-    
-    let div2b1 = document.createElement('div');
-    div2b1.innerHTML = postContent.subreddit_name_prefixed;
-    div2b1.style = "display: inline-block; font-weight: 900; font-size: 12px";
-    document.getElementById('div2b_' + i).appendChild(div2b1);
-    
-    let div2b2 = document.createElement('div');
-    div2b2.innerHTML = postContent.author + " " + postContent.created_utc;
-    div2b2.style = "display: inline-block; font-size: 12px; color: gray";
-    document.getElementById('div2b_' + i).appendChild(div2b2);
-    
-    let div3 = document.createElement('div');
-    div3.innerHTML = postContent.num_comments;
-    div3.style = "display: inline-block; height: 90px; width: 35px; font-size: 12px; position: relative; top: -60px; font-weight: bold; color: gray; border: solid";
-    document.getElementById('postContainer' + i).appendChild(div3);
   }
 };
-userAction3();
+buildStructure();
+
+const buildContent = () => {
+  for (let i = 1; i <= jsonChildren.length; i++) {
+    let data = jsonChildren[i - 1].data;
+
+    let scoreDiv = document.getElementById("postSection_" + i + "_" + 1);
+    let scoreSpan = document.createElement('span');
+    scoreSpan.innerHTML = data.score;
+    scoreSpan.id = "upvotes_" + i;
+    scoreDiv.appendChild(scoreSpan);
+
+    let typeDiv = document.getElementById("postSection_" + i + "_" + 2);
+    let typeSpan = document.createElement('span');
+    typeSpan.innerHTML = data.post_hint;
+    typeSpan.id = "type_" + i;
+    typeDiv.appendChild(typeSpan);
+
+    let topSubDiv = document.getElementById("subDiv_" + i + "_" + 3 + "_" + 1);
+    let topSpan = document.createElement('span');
+    topSubDiv.appendChild(topSpan);
+
+    let titleSpan = document.createElement('span');
+    titleSpan.innerHTML = data.title;
+    titleSpan.id = "title_" + i;
+    topSpan.appendChild(titleSpan);
+
+    let linkSpan = document.createElement('span');
+    linkSpan.innerHTML = data.url;
+    linkSpan.id = "link_" + i;
+    topSpan.appendChild(linkSpan);
+
+    let bottomSubDiv = document.getElementById("subDiv_" + i + "_" + 3 + "_" + 2);
+    let bottomSpan = document.createElement('span');
+    bottomSubDiv.appendChild(bottomSpan);
+
+    let subredditSpan = document.createElement('span');
+    subredditSpan.innerHTML = data.subreddit_name_prefixed;
+    subredditSpan.id = "subreddit_" + i;
+    bottomSpan.appendChild(subredditSpan);
+
+    let authorSpan = document.createElement('span');
+    authorSpan.innerHTML = data.author;
+    authorSpan.id = "author_" + i;
+    bottomSpan.appendChild(authorSpan);
+
+    let createdSpan = document.createElement('span');
+    createdSpan.innerHTML = data.created_utc;
+    createdSpan.id = "created_" + i;
+    bottomSpan.appendChild(createdSpan);
+
+    let commentsDiv = document.getElementById("postSection_" + i + "_" + 4);
+    let commentsSpan = document.createElement('span');
+    commentsSpan.innerHTML = data.num_comments;
+    commentsSpan.id = "comments_" + i;
+    commentsDiv.appendChild(commentsSpan);
+  }
+};
+buildContent();
+
+const buildStyle = () => {
+  for (let i = 1; i <= jsonChildren.length; i++) {
+    let container = document.getElementById("postContainer_" + i).style;
+    container.display = "flex";
+    // container["justify-content"] = "space-evenly";
+    container["align-items"] = "flex-start";
+    // comment these out for actual build out
+    container.border = "solid";
+
+    for (let j = 1; j <= 4; j++) {
+      let upvotesDiv = document.getElementById("postSection_" + i + "_" + 1).style;
+      upvotesDiv.width = "90px";
+      upvotesDiv.height = "24px";
+      upvotesDiv.display = "flex";
+      upvotesDiv["justify-content"] = "center";
+      upvotesDiv["align-items"] = "center";
+
+      let typeDiv = document.getElementById("postSection_" + i + "_" + 2).style;
+      typeDiv.width = "40px";
+      typeDiv.height = "24px";
+      typeDiv.display = "flex";
+      typeDiv["justify-content"] = "center";
+      typeDiv["align-items"] = "center";
+
+      let mainContentDiv = document.getElementById("postSection_" + i + "_" + 3).style;
+      mainContentDiv.display = "flex";
+      mainContentDiv["flex-flow"] = "column";
+
+      let commentsDiv = document.getElementById("postSection_" + i + "_" + 4).style;
+      commentsDiv.width = "70px";
+      commentsDiv.height = "24px";
+      commentsDiv.display = "flex";
+      commentsDiv["justify-content"] = "center";
+      commentsDiv["align-items"] = "center";
+      commentsDiv["margin-left"] = "auto";
+    }
+  };
+};
+buildStyle();
+
+const styleContent = () => {
+  let body = document.getElementsByTagName("body")[0].style;
+  body["font-family"] = "sans-serif";
+  body["font-size"] = "14px";
+
+  for (let i = 1; i <= jsonChildren.length; i++) {
+    let upvotes = document.getElementById("upvotes_" + i).style;
+    upvotes["font-weight"] = 600;
+
+    let type = document.getElementById("type_" + i).style;
+    type.color = "rgb(135, 138, 140)";
+
+    let title = document.getElementById("title_" + i).style;
+    title["font-size"] = "14px";
+    title.padding = "5px";
+
+    let link = document.getElementById("link_" + i).style;
+    link.color = "#0079d3";
+
+    let subreddit = document.getElementById("subreddit_" + i).style;    
+    subreddit["font-weight"] = 800;
+    subreddit.padding = "5px";
+
+    let author = document.getElementById("author_" + i).style;
+    author.color = "#787c7e";
+
+    let created = document.getElementById("created_" + i).style;
+    created.color = "#787c7e";
+    created.padding = "5px";
+
+    let comments = document.getElementById("comments_" + i).style;
+    comments["font-weight"] = 600;
+    comments.color = "#878a8c";
+  }
+};
+styleContent();
+
